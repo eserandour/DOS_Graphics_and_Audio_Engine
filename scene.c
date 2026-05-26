f042ebf06383d3d22bde934f7b2259a856cb19a3
3615 <internet-1@serandour.org>
2026-05-26 23:40:40 +0200

    Add files via upload

--- scene.c
+++ scene.c
@@ -1,5 +1,5 @@
 /* =========================================================
-   SCENE.C — Gestionnaire de scènes
+   SCENE.C — Gestionnaire de scenes
    ========================================================= */
 
 #include "timer.h"
@@ -9,6 +9,7 @@ void scene1(void);
 void scene2(void);
 void scene3(void);
 void scene4(void);
+void scene5(void);
 
 Scene currentScene = SCENE_1;
 unsigned long sceneStart = 0;
@@ -20,6 +21,7 @@ static SceneFunc scenes[] = {
     scene2,  /* SCENE_2 = 1 */
     scene3,  /* SCENE_3 = 2 */
     scene4,  /* SCENE_4 = 3 */
+    scene5,  /* SCENE_5 = 4 */
 };
 
 void setScene(Scene s)
